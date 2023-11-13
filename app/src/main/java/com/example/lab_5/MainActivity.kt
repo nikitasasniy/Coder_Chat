4180747cd0b80808a579408f4c9acdfe2859860d
nikitasasniy <nikitasil@mail.ru>
2023-11-13 22:47:02 +0500

    ветка для меня

--- app/src/main/java/com/example/lab_5/MainActivity.kt
+++ app/src/main/java/com/example/lab_5/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting1(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "git $name!",
+        text = "giasdasdasdasdt $name!",
         modifier = modifier
     )
 }
