881933daf053fe474097d40e052239eaec39ddd8
nikitasasniy <nikitasil@mail.ru>
2023-12-06 19:02:44 +0500

    vers 0.1

--- app/src/androidTest/java/com/example/lab_5/ConversationTest.kt
+++ app/src/androidTest/java/com/example/lab_5/ConversationTest.kt
@@ -4,10 +4,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.lab_5.Conversation
-import com.example.lab_5.Message
-import com.example.lab_5.MessageCard
-import com.example.lab_5.UserInfo
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,14 +17,11 @@ class ConversationTest {
     @OptIn(ExperimentalMaterial3Api::class)
     @Test
     fun conversationTest() {
-        // Given
         val messages = listOf(
             Message("user1", "Hello"),
-//            Message("user2", "Hi there!")
         )
         val userInfos = listOf(
             UserInfo("user1", "John"),
-//            UserInfo("user2", "Jane")
         )
 
         // When
@@ -41,18 +34,67 @@ class ConversationTest {
             )
         }
 
-        // Then
-        // Assuming your IconButton for logout has contentDescription "Logout"
+        // проверяем кнопку logoutButton
         composeTestRule.onNodeWithTag("logoutButton").assertIsDisplayed()
 
-        // Assuming your message cards have specific text content
-//        composeTestRule.onNodeWithTag("messageCard1").assertIsDisplayed()
-//        composeTestRule.onNodeWithTag("messageCard2").assertIsDisplayed()
-
-        // Assuming your TextField has a label "Введите сообщение"
+        // проверяем возможность заполнения поля
         composeTestRule.onNodeWithTag("messageTextField").assertIsDisplayed()
 
-        // Assuming your IconButton for send has contentDescription "Send"
+        // проверяем кнопку sendButton
         composeTestRule.onNodeWithTag("sendButton").assertIsDisplayed()
     }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Test
+    fun messagesDisplayTest() {
+        val messages = listOf(
+            Message("user1", "Hello"),
+            Message("user2", "Hi there!")
+        )
+        val userInfos = listOf(
+            UserInfo("user1", "John"),
+            UserInfo("user2", "Jane")
+        )
+
+        composeTestRule.setContent {
+            Conversation(
+                messages = messages,
+                userInfos = userInfos,
+                onSendMessage = {},
+                onLogout = {}
+            )
+        }
+
+        // Проверяем, что количество карточек сообщений соответствует количеству сообщений
+        composeTestRule.onNodeWithTag("messageCardFalse").equals(messages.size)
+
+        composeTestRule.onNodeWithTag("IconButton").assertIsDisplayed()
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Test
+    fun conversationSendMessageTest() {
+        var sendMessageCalled = false
+
+        // Given
+        val messages = emptyList<Message>()
+        val userInfos = emptyList<UserInfo>()
+
+        // When
+        composeTestRule.setContent {
+            Conversation(
+                messages = messages,
+                userInfos = userInfos,
+                onSendMessage = {
+                    sendMessageCalled = true
+                },
+                onLogout = {}
+            )
+        }
+
+        // Then
+        composeTestRule.onNodeWithTag("messageTextField").performTextInput("Hello, testing!")
+        composeTestRule.onNodeWithTag("sendButton").performClick() // проверяем отправку текста
+    }
 }
+
--- app/src/androidTest/java/com/example/lab_5/MessageCardTest.kt
+++ app/src/androidTest/java/com/example/lab_5/MessageCardTest.kt
@@ -16,18 +16,59 @@ class MessageCardTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun messageCardExpandedTest() {
+    fun messageCardDisplayTest() {
+        val message = Message("user1", "Hello, this is a long message.")
+        val userInfos = listOf(UserInfo("user1", "John"))
+
+        composeTestRule.setContent {
+            MessageCard(message, userInfos, isExpanded = { false })
+        }
+
+        composeTestRule.onNodeWithText("John").assertIsDisplayed() // отображение ника
+        composeTestRule.onNodeWithText("Hello, this is a long message.").assertIsDisplayed() // отображение сообщения
+
+    }
+
+    @Test
+    fun messageCardClickTest() {
         // Given
-        val message = Message("user", " test.")
+        val message = Message("user1", "Hello, this is a message.")
+        val userInfos = listOf(UserInfo("user1", "John"))
+
         // When
         composeTestRule.setContent {
-            MessageCard(msg = message, userInfos = emptyList())
+            MessageCard(message, userInfos, isExpanded = { false })
         }
+
         // Then
-        composeTestRule.onNodeWithText(" test.").assertDoesNotExist()
-        // When
-        composeTestRule.onNodeWithText(" test.").performClick()
-        // Then
-        composeTestRule.onNodeWithText(" test.").assertIsDisplayed()
+        // Проверим, что изначально isExpanded = false
+        composeTestRule.onNodeWithTag("messageCardFalse").assertIsDisplayed()
+
+        // Вызываем performClick() для имитации клика
+        composeTestRule.onNodeWithTag("messageCardFalse").performClick()
+
+        // Даем немного времени для анимации
+        Thread.sleep(1000)
+
+        // Проверим, что после клика isExpanded = true
+        composeTestRule.onNodeWithTag("textAnimate").assertIsDisplayed()
+//        Thread.sleep(5000)
+//        composeTestRule.onNodeWithTag("textExtensionAnimate").assertIsDisplayed()
     }
+
+    @Test
+    fun messageCardEmptyTextTest() {
+        val emptyMessage = Message("user1", "")
+        val userInfos = listOf(UserInfo("user1", "John"))
+
+        composeTestRule.setContent {
+            MessageCard(emptyMessage, userInfos, isExpanded = { false })
+        }
+
+        composeTestRule.onNodeWithText("Hello, this is a long message.").assertDoesNotExist() // проверка отображения пустого сообщения
+    }
+
+
+
+
 }
